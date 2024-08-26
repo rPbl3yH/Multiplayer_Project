f32fadf9a5ff51e939dab7b1ec8ad3bc5e47e450
rPbl3yH <tnv2205@gmail.com>
2024-08-26 06:34:37 +0300

    feat: add jump

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Game.Core
@@ -6,12 +7,45 @@ namespace Game.Core
     {
         [SerializeField] private Rigidbody _rigidbody;
         [SerializeField] private float _speed = 3f;
+        [SerializeField] private Transform _head;
+        [SerializeField] private Transform _cameraPoint;
+
+        [SerializeField] private float _minHeadAngle = -90f;
+        [SerializeField] private float _maxHeadAngle = 90f;
+
+        [Header("Jump")]
+        [SerializeField] private float _jumpForce = 50f;
         
         private Vector3 _moveDirection;
+        private float _yRotate;
+        private float _xRotate;
+
+        private bool _isGrounded;
+
+        private void Start()
+        {
+            var cameraTransform = Camera.main.transform;
+            cameraTransform.SetParent(_cameraPoint);
+            cameraTransform.localPosition = Vector3.zero;
+            cameraTransform.localRotation = Quaternion.identity;
+        }
 
         private void FixedUpdate()
         {
             Move();
+            Rotate();
+        }
+
+        public void Rotate(float xAngle)
+        {
+            _xRotate = Mathf.Clamp(_xRotate + xAngle, _minHeadAngle, _maxHeadAngle);
+            _head.localEulerAngles = new Vector3(_xRotate, 0f, 0f);
+        }
+
+        public void SetInput(Vector3 moveDirection, float yRotate)
+        {
+            _moveDirection = moveDirection;
+            _yRotate += yRotate;
         }
 
         private void Move()
@@ -19,21 +53,51 @@ namespace Game.Core
             var velocity = _moveDirection.z * transform.forward + _moveDirection.x * transform.right;
             velocity.Normalize();
             velocity *= _speed;
+            
+            velocity.y = _rigidbody.velocity.y;
+            
             _rigidbody.velocity = velocity;
             
             // _rigidbody.velocity = _moveDirection * _speed;
             // transform.position += _moveDirection * _speed * Time.deltaTime;
         }
-        
-        public void SetupMoveDirection(Vector3 moveDirection)
+
+        private void Rotate()
         {
-            _moveDirection = moveDirection;
+            _rigidbody.angularVelocity = new Vector3(0f, _yRotate, 0f);
+            _yRotate = 0f;
         }
 
         public void GetMoveInfo(out Vector3 position, out Vector3 velocity)
         {
             position = transform.position;
             velocity = _rigidbody.velocity;
+        }
+
+        public void Jump()
+        {
+            if (!_isGrounded)
+            {
+                return;
+            }
+            
+            _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.VelocityChange);
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            foreach (ContactPoint contactPoint in other.contacts) 
+            {
+                if (contactPoint.normal.y > 0.45f)
+                {
+                    _isGrounded = true;
+                }
+            }
+        }
+
+        private void OnCollisionExit(Collision other)
+        {
+            _isGrounded = false;
         }
     }
 }
--- Assets/Game/Scripts/PlayerInput.cs
+++ Assets/Game/Scripts/PlayerInput.cs
@@ -7,6 +7,7 @@ namespace Game.Core
     public class PlayerInput : MonoBehaviour
     {
         [SerializeField] private Player _player;
+        [SerializeField] private float _mouseSensitivity = 2f;
         
         private void Update()
         {
@@ -15,7 +16,16 @@ namespace Game.Core
 
             var direction = new Vector3(horizontalAxis, 0f, verticalAxis).normalized;
             
-            _player.SetupMoveDirection(direction);
+
+            var mouseX = Input.GetAxis("Mouse X");
+            var mouseY = Input.GetAxis("Mouse Y");
+            _player.SetInput(direction, mouseX);
+            _player.Rotate(-mouseY * _mouseSensitivity);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _player.Jump();
+            }
             
             SendMoveInfo();
         }
